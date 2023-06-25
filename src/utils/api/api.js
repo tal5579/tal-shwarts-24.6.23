567acc084f940e219d37ad4f08c39b13c5d50a26
Tal Shwartz <tal5579@gmail.com>
2023-06-25 22:58:48 +0300

    add headers axios

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://dataservice.accuweather.com/';
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
 axios.defaults.params = {}
 axios.defaults.params['apikey'] = 'alDLATG0Vh0gAc9t9vK7RhcXAGfGKSGg';
 axios.defaults.params['language'] = 'en-us';
+axios.defaults.withCredentials = true;
 
 export default axios;

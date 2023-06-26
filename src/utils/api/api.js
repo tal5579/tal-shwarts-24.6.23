f37b3a8fda030f6f5a13eda318a710f55519174c
Tal Shwartz <tal5579@gmail.com>
2023-06-26 10:08:59 +0300

    fix axios configurations

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://dataservice.accuweather.com/';
-axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
 axios.defaults.params = {}
-axios.defaults.params['apikey'] = 'alDLATG0Vh0gAc9t9vK7RhcXAGfGKSGg';
+axios.defaults.params['apikey'] = 'DGvbkSMIlATNAW4wkxp1k3qVgH23Uive';
 axios.defaults.params['language'] = 'en-us';
-axios.defaults.withCredentials = true;
 
 export default axios;
